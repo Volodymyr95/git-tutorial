14e857f462918a10a0ae56a72f392a8b09eb98c4
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2023-06-16 19:27:32 +0300

    fixed PR comments: * move logic from Main class to Menu class

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
-
-    private static final int EXIT_NUMBER = 0;
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        boolean isRunning = true;
+        new Menu().run();
 
-        while (isRunning) {
-            int menuItem = scanner.nextInt();
-            if (menuItem == EXIT_NUMBER) {
-                isRunning = false;
-            }
-        }
     }
 }
